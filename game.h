985870a07f9f7ec3e6e0b75e3a235d5a2e00a406
Rienk <rienk2001@hotmail.com>
2021-01-13 15:49:41 +0100

    Draw is concurrent

--- game.h
+++ game.h
@@ -20,7 +20,11 @@ class Game
     void tick(float deltaTime);
     void merge_sort(std::vector<Tank*>& sorted_tanks, int start, int end, std::atomic<int>& threads);
     void merge(std::vector<Tank*>& sorted_tanks, int start, int middle, int end);
-
+    void draw_tanks();
+    void draw_rockets();
+    void draw_smokes();
+    void draw_particle_beams();
+    void draw_explosions();
     void test();
 
     
